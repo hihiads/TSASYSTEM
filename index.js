0ef9be569c9e7ba2069efc2e16e004395e8b853f
hihiads <98236975+hihiads@users.noreply.github.com>
2025-11-12 21:31:25 +0100

    Integrate Express server with Discord bot
    Added an Express server to the Discord bot.

--- index.js
+++ index.js
@@ -313,7 +313,18 @@ function sendLog(guild, text) {
   if (logChannel) logChannel.send(text);
 }
 
+const express = require("express");
+const app = express ();
+
+app.listen(3000, () => {
+  console.log("Project is running!")
+})
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
 
 
 
 client.login(process.env.token);
+
